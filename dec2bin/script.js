5c31413dce66c1690521125a502d14682a6a78f5
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2021-08-16 15:11:20 -0700

    add js functionality

--- dec2bin/script.js
+++ dec2bin/script.js
@@ -1,1 +1,22 @@
+const binaryInput = document.querySelector('.js-binary-input');
+const decimalInput = document.querySelector('.js-decimal-input');
 
+const setMaxMin = (value) => (value.maxLength = 21);
+
+[decimalInput, binaryInput].forEach((el) => setMaxMin(el));
+
+binaryInput.addEventListener('input', () => {
+  const num = +binaryInput.value;
+
+  decimalInput.value = parseInt(num, 2);
+
+  if (num === 0) decimalInput.value = '';
+});
+
+binaryInput.addEventListener('focus', () =>
+  decimalInput.classList.add('active')
+);
+
+binaryInput.addEventListener('focusout', () =>
+  decimalInput.classList.remove('active')
+);
